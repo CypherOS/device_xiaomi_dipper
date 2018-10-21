8c06ab18daa54dfe92ddd55dcb07dcc6f97c7887
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-21 15:05:35 -0300

    dipper: Implement libinit
    * Use pixel fingerprint to bypass safetynet
    
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

--- aoscp.mk
+++ aoscp.mk
@@ -16,10 +16,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:9/PKQ1.180729.001/V10.3.2.0.PEAMIXM:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="dipper-user 9 PKQ1.180729.001 V10.3.2.0.PEAMIXM release-keys" \
     PRODUCT_NAME="dipper" \
     TARGET_DEVICE="dipper"
 
